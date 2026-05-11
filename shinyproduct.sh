f7dd7ad480eca8c52a04cb27b622fe92920149e5
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-05-11 23:43:28 +0200

    update shinyproduct

--- shinyproduct.sh
+++ shinyproduct.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Try to build the product
 
+# Cleanup
+rm -r ~/gbuild/gfs
+rm -r ~/gbuild/image
+rm -r ~/gbuild/localpc/ensemble
+
 # Build product
-rm -r ~/gbuild
 cd ~/pcgeos/Tools/build/product/bbxensem/Scripts
 perl -I. buildbbx.pl
